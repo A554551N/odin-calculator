ef513faa11e972131ad3a4345d47f800fe8da2f9
Justin Gordon <linuxfreak1030@gmail.com>
2025-03-15 12:08:51 -0400

    implement functional display and clear button

--- script.js
+++ script.js
@@ -24,8 +24,26 @@ const operate = function(a,b,operator) {
     return operations[operator](a,b);
 }
 
+function updateDisplay(content) {
+    // Take in an array object, join it and update the display;
+    const display = document.querySelector(".input");
+    display.textContent = content.join("");
+}
+
 let numA = 0;
 let numB = 0;
 let operator = "";
+let userInput = []
 
-console.log(operate(3,2,"mult"))
+const display = document.querySelector(".input");
+const numberContainer = document.querySelector(".numberButtonContainer");
+numberContainer.addEventListener("click",(e) => {
+    userInput.push(e.target.id)
+    updateDisplay(userInput)
+})
+// each digit pressed pushes onto an array userInput, displayed on screen joined
+// store the operator button id pressed in operator, join the values of userInput, convert to Int and store in numA, then empty userInput (userInput = [])
+
+// each digit pressed pushes onto an array userInput, displayed on screen joined
+
+//when = is pressed, join values of userInput, store in numB, clear userInput, call operate() and return the result of the calculation to the screen
